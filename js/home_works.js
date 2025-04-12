2ba0edc781a48009a6dee33d9621a336f54fc7c3
adyujum <jumanazarovaadila@gmail.com>
2025-04-13 03:28:30 +0600

    dgg

--- js/home_works.js
+++ js/home_works.js
@@ -99,3 +99,67 @@ const resetTimer = () => {
 document.querySelector('#start').addEventListener('click', startTimer)
 document.querySelector('#stop').addEventListener('click', stopTimer)
 document.querySelector('#reset').addEventListener('click', resetTimer)
+
+
+//character 
+
+const characterLst = document.querySelector(".characters-list")
+
+const xhr = new XMLHttpRequest()
+xhr.open('GET','../data/persons.json')
+
+xhr.onload = function () {
+    if(xhr.status === 200){
+        const persons = JSON.parse(xhr.responseText)
+        persons.forEach(person => {
+            const characterCard = document.createElement('div');
+            characterCard.className = 'character-card';
+
+            const characterPhoto = document.createElement('div');
+            characterPhoto.className = 'character-photo';
+
+            const characterPhotoImg = document.createElement('img');
+            characterPhotoImg.src = person.person_photo;
+            characterPhotoImg.alt = person.name;
+
+            characterPhoto.appendChild(characterPhotoImg);
+
+            characterCard.appendChild(characterPhoto);
+
+            const h3 = document.createElement('h3');
+            h3.innerHTML = person.name;
+            characterCard.appendChild(h3);
+
+            const house = document.createElement('p');
+            house.innerHTML = person.what_a_ninja;
+            characterCard.appendChild(house);
+
+            const age = document.createElement('p');
+            age.innerHTML = `Age: ${person.age}`;
+            characterCard.appendChild(age);
+
+
+
+            characterLst.appendChild(characterCard);
+        });
+    }else {
+        console.log('Ошибка при загрузке!');
+        
+    }
+}
+
+xhr.send()
+
+const xhr2 = new XMLHttpRequest()
+xhr2.open('GET','./data/any.json')
+
+xhr2.onload = () => {
+    if (xhr2.status === 200) {
+        const data = JSON.parse(xhr2.responseText);
+        console.log('Данные :', data);
+    } else {
+        console.log('Ошибка при загрузке данных');
+    }
+}
+
+xhr2.send()
